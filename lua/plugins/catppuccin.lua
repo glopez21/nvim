93a4268fdb8cc9d09fbdbcfd0cdd053eb3dfe1cd
glopez21 <genghis.lopez@gmail.com>
2024-09-14 19:17:07 -0400

    catppuccin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,6 +5,45 @@ return {
       name = "catppuccin",
       priority = 1000,
       config = function()
+
+        catppuccin.setup({
+          flavour = "mocha",
+          term_colors = true,
+          styles = {
+              conditionals = {},
+              functions = {"italic"},
+              types = {"bold"}
+          },
+          color_overrides = {
+              mocha = {
+                  base = "#171717", -- background
+                  surface2 = "#9A9A9A", -- comments
+                  text = "#F6F6F6"
+              }
+          },
+          highlight_overrides = {
+              mocha = function(C)
+                  return {
+                      NvimTreeNormal = {bg = C.none},
+                      CmpBorder = {fg = C.surface2},
+                      Pmenu = {bg = C.none},
+                      NormalFloat = {bg = C.none},
+                      TelescopeBorder = {link = "FloatBorder"}
+                  }
+              end
+          },
+          integrations = {
+              barbar = true,
+              cmp = true,
+              gitsigns = true,
+              native_lsp = {enabled = true},
+              nvimtree = true,
+              telescope = true,
+              treesitter = true,
+              treesitter_context = true
+          }
+      })
+      
         vim.cmd.colorscheme "catppuccin"
       end
     }
