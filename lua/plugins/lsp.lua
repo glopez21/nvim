a216dd849c194a1d41b8c16ca0fe5686cf23d94d
glopez <glopez21@live.com>
2025-04-08 12:38:49 -0400

    removed the rustacean section on the lsp.lua setting

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,7 +14,7 @@ return {
         },
      },
 
-     {
+      --[[{
         'mrcjkb/rustaceanvim',
         version = '^5', -- Recommended
         lazy = false, -- This plugin is already lazy
@@ -22,7 +22,7 @@ return {
         config = function ()
           local mason_registry = require('mason-registry')
           -- local codelldb = mason_registry.get_package("codelldb")
-          local extension_path = codelldb:get_install_path() .. "/extension/"
+          -- local extension_path = codelldb:get_install_path() .. "/extension/"
           -- local codelldb_path = extension_path .. "adapter/codelldb"
           local liblldb_path = extension_path.. "lldb/lib/liblldb.dylib"
           local cfg = require('rustaceanvim.config')
@@ -33,7 +33,7 @@ return {
             },
           }
         end
-      },
+      },]]
     
       {
         'rust-lang/rust.vim',
