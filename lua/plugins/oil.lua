e6c711c900ff999a7dc393c17dd793c455bc87b1
glopez <genghis.lopez@gmail.com>
2025-04-12 10:01:10 -0400

    added/enhanced oil.lua

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,9 +1,11 @@
 return {
     "stevearc/oil.nvim",
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons", },
     config = function()
-      local oil = require("oil")
-      oil.setup()
-      vim.keymap.set("n", "-", oil.toggle_float, {})
+      require("oil").setup({default_file_explorer = true, })
+      -- vim.keymap.set("n", "-", oil.toggle_float, {})
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     end,
   }
   
