78973c66b15bda21332501c2131de7627a77aa0c
glopez <genghis.lopez@gmail.com>
2025-04-12 15:22:19 -0400

    added comment, modified harpoon and added markdoen preview

---
+++ lua/plugins/markdow-preview.lua
@@ -0,0 +1,16 @@
+return {
+    "iamcco/markdown-preview.nvim",
+
+    keys = {
+        {
+          "<leader>mp",
+          ft = "markdown",
+          "<cmd>MarkdownPreviewToggle<cr>",
+          desc = "Markdown Preview",
+        },
+      },
+    
+    -- config = function()
+    --     require().setup()
+    -- end
+}
